fea8800327a31e8cd27324e13e0d0137356c5147
Zhou Kevin Yu <Zhoukevinyu1@gmail.com>
2022-06-10 13:29:39 +1000

    Added stubs

--- src/channel.js
+++ src/channel.js
@@ -5,3 +5,7 @@ function channelJoinV1(authUserId, channelId) {
 function channelInviteV1(authUserId, channelId, uId) {
     return authUserId + channelId + uId;
 }
+
+function channelDetailsV1(authUserId, channelId) {
+    return authUserId + channelId;
+}
