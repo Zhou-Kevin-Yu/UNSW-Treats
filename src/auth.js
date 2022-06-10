b01a2339729f134741b674507d38852933dcb905
etkintetik <etkintetik101@gmail.com>
2022-06-10 12:57:01 +1000

    added stub for authRegisterV1

---
+++ src/auth.js
@@ -0,0 +1,5 @@
+// Stub for a function 'authRegisterV1', with arguements named 'email', 'password', 'nameFirst' and 'nameLast'
+// Returns a string concatenating the name of 'email', 'password', 'nameFirst' and 'nameLast'
+function authRegisterV1(email, password, nameFirst, nameLast) {
+    return 'email' +  'password' +  'nameFirst' + 'nameLast';
+}
