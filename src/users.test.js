32b56689e653bdc260a332013e72ec7708e22162
etkintetik <etkintetik101@gmail.com>
2022-06-19 20:01:42 +1000

    fixed the tests for userse

--- src/users.test.js
+++ src/users.test.js
@@ -3,32 +3,28 @@ import { authRegisterV1 } from "./auth";
 
 describe('userProfileV1 - testing invalid operations', () => {
     test('Invalid authUserId and uId', () => {
-        expect(() => {
-            userProfileV1(1, 1);
-        }).toStrictEqual({ error: 'error' });
+        console.log("Start of the test");
+        const user = userProfileV1(1, 1);
+        expect(user).toStrictEqual({ error: 'error' });
     });
 
     test('Invalid authUserId', () => {
         const {uId} = authRegisterV1('gary.sun@student.unsw.edu.au', 'bird27', 'Gary', 'Sun');
-        expect(() => {
-            userProfileV1(null, uId);
-        }).toStrictEqual({ error: 'error' });
+        const user = userProfileV1(null, uId);
+        expect(user).toStrictEqual({ error: 'error' });
     });
 
     test('Invalid uId', () => {
         const {authUserId} = authRegisterV1('gary.sun@student.unsw.edu.au', 'bird27', 'Gary', 'Sun');
-        expect(() => {
-            userProfileV1(authUserId, null);
-        }).toStrictEqual({ error: 'error' });
+        const user = userProfileV1(authUserId, null);
+        expect(user).toStrictEqual({ error: 'error' });
     });
 });
 
 describe('userProfileV1 - testing valid operations', () => {
     test('Valid authUserId and uId = authUserId', () => {
         const {authUserId} = authRegisterV1('gary.sun@student.unsw.edu.au', 'bird27', 'Gary', 'Sun');
-        expect(() => {
-            userProfileV1(authUserId, authUserId);
-        }).toStrictEqual({
+        expect(userProfileV1(authUserId, authUserId)).toStrictEqual({
             uId: authUserId,
             email: 'gary.sun@student.unsw.edu.au',
             nameFirst: 'Gary',
@@ -40,9 +36,7 @@ describe('userProfileV1 - testing valid operations', () => {
     test('Valid authUserId and uId', () => {
         const {authUserId} = authRegisterV1('gary.sun@student.unsw.edu.au', 'bird27', 'Gary', 'Sun');
         const {uId} = authRegisterV1('manav.pawar@student.unsw.edu.au', 'mjerry', 'Manav', 'Pawar');
-        expect(() => {
-            userProfileV1(authUserId, uId);
-        }).toStrictEqual({
+        expect(userProfileV1(authUserId, uId)).toStrictEqual({
             uId: uId,
             email: 'manav.pawar@student.unsw.edu.au',
             nameFirst: 'Manav',
