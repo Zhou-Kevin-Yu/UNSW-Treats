6813e0cd64b0b4111f4345ed79bb1be1de36ac07
Calvin Xu <z5359028@ad.unsw.edu.au>
2022-06-19 17:56:11 +1000

    Updated channelInviteV1 test by fixing errors and typos

--- src/channelInviteV1.test.js
+++ src/channelInviteV1.test.js
@@ -1,54 +1,64 @@
-import { channelsCreateV1, channelsListallV1} from './channels.js';
-import { channelInviteV1, channelDetailsV1 } from './channel.js';
-import { userProfileV1 }          from './users.js';
-
+import { channelsCreateV1 }                     from './channels.js';
+import { channelInviteV1, channelDetailsV1 }    from './channel.js';
+import { userProfileV1 }                        from './users.js';
+import { clearV1 }                              from './other.js';
 
 // If channelInviteV1 was successful
 test('Test successful channelInviteV1', () => {
-    const noErrorOutput = 1;
     const validChannelId = channelInviteV1(1, 2, 1);
-    expect(validChannelId).toEqual(noErrorOutput);
-    
-    // or is it expect(validChannelId).toEqual(' '); because a success case returns nothing?
+    const empty = {};
+    expect(validChannelId).toEqual(empty);
 });
 
 // If the channelId does not refer to a valid channel 
-test('Test wrong channelId', () => {
-    const input = channelInviteV1(1, undefined, 2);
-    const checkChannelId = channelsListallV1(1);
-     // However in the spec it says channels is an array of objects?? do I use .toMatchObject then??
-    expect(input).toEqual(expect.not.arrayContaining(checkChannelId));
-    
-    // can you do const errorOutput = expect(input).toEqual(expect.not.arrayContaining(channels));
-    // then expect(errorOutput).toStrictEqual({error: 'error'})
-    // or expect(input).toStrictEqual({error: 'error'})
+describe ('Testing invalid channelId', () => {
+    test('Test undefined channelId', () => {
+        const input = channelInviteV1(1, undefined, 2);
+        expect(input).toStrictEqual({error: 'error'})
+    });
+   
+    clearV1();
+    test('Test wrong channelId', () => {
+        let checkChannelId = channelsCreateV1(1, 'Calvin', true);
+        checkChannelId = checkChannelId + 1;
+        
+        const input = channelInviteV1(1, 1, 2);
+        expect(input).toStrictEqual({error: 'error'})
+
+        checkChannelId = checkChannelId - 2;
+        expect(input).toStrictEqual({error: 'error'})
+    });
 });
 
 // If the uId does not refer to a valid user
-test('Test wrong uId', () => {
-    const input = channelInviteV1(1, 1, undefined);
-    const checkUserId = userProfileV1(1, 2);
-    expect(input).toEqual(expect.not.objectContaining(checkUserId));
+describe ('Testing invalid uId', () => {
+    test('Test undefined uId', () => {
+        const input = channelInviteV1(1, 1, undefined);
+        expect(input).toStrictEqual({error: 'error'})
+    });
+
+    clearV1();
+    test('Test wrong uId', () => {
+        let checkUserId = userProfileV1(1, 3);
+        const input = channelInviteV1(1, 1, 2);
+        expect(input).toStrictEqual({error: 'error'})
+
+        checkUserId = userProfileV1(1, 1);
+        expect(input).toStrictEqual({error: 'error'})
+    });
 });
+
 
 // If the user is already a member of the channel
 test('Test uId that is already in channel', () => {
+    const checkIfMember = channelDetailsV1(2, 1);
     const input = channelInviteV1(1, 1, 2);
-    const checkIfMember = channelDetailsV1(1, 1);
-    
-    // Checking if uId is already in the allMembers array
-    expect(input).toEqual(expect.arrayContaining(checkIfMember));
+    expect(input).toStrictEqual({error: 'error'})
 });
 
 // If the channelId is valid but authorised user is not a member
-test('Test uId that is already in channel', () => {
-    const input = channelInviteV1(1, 1, 2);
-    const checkIfAuthMember = channelDetailsV1(2, 1);
-    
-    // Checking if authId is not in the allMembers array
-    expect(input).toEqual(expect.not.arrayContaining(checkIfAuthMember));
+test('Test authId that is not a member of the channel', () => {
+    const checkIfAuthMember = channelDetailsV1(3, 1);
+    const input = channelInviteV1(5, 1, 4);
+    expect(input).toStrictEqual({error: 'error'})
 });
-
-
-
-
