fd13eae2379f1d6199f9e57d10e4590d2e3b7904
Calvin Xu <z5359028@ad.unsw.edu.au>
2022-06-19 18:12:37 +1000

    Updated channelMessagesV1 tests by fixing errors

--- src/channelMessagesV1.test.js
+++ src/channelMessagesV1.test.js
@@ -1,5 +1,6 @@
-import { channelsListallV1} from './channels.js';
+import { channelsCreateV1 } from './channels.js';
 import { channelDetailsV1, channelMessagesV1} from './channel.js';
+import { clearV1 }                              from './other.js';
 
 // If channelMessagesV1 was successful
 test('Test successful channelMessagesV1', () => {
@@ -9,22 +10,29 @@ test('Test successful channelMessagesV1', () => {
     expect(input).toStrictEqual([], 0, -1);
 });
 
-// If the channelId does not refer to a valid channel
-test('Test wrong channelId', () => {
-    const input = channelMessagesV1(1, undefined, 0);
-    const checkChannelId = channelsListallV1(1);
-     // However in the spec it says channels is an array of objects?? do I use .toMatchObject then??
-    expect(input).toEqual(expect.not.arrayContaining(checkChannelId));
-    
-    // can you do const errorOutput = expect(input).toEqual(expect.not.arrayContaining(checkChannelId));
-    // then expect(errorOutput).toStrictEqual({error: 'error'})
-    // or expect(input).toStrictEqual({error: 'error'})
+// If the channelId does not refer to a valid channel 
+describe ('Testing invalid channelId', () => {
+    test('Test undefined channelId', () => {
+        const input = channelMessagesV1(1, undefined, 0);
+        expect(input).toStrictEqual({error: 'error'})
+    });
+   
+    clearV1();
+    test('Test wrong channelId', () => {
+        let checkChannelId = channelsCreateV1(1, 'Calvin', true);
+        checkChannelId = checkChannelId + 1;
+        
+        const input = channelMessagesV1(1, 1, 0);
+        expect(input).toStrictEqual({error: 'error'})
+
+        checkChannelId = checkChannelId - 2;
+        expect(input).toStrictEqual({error: 'error'})
+    });
 });
 
 // If start is greater than the total number of messages in the channel
 test('Test if start value is greater than total number of messages', () => {
     const input = channelMessagesV1(1, 1, 1000);
-    // is there a limit on how many total messages there could be
     
     // Error as message array will be empty in Iteration 1
     expect(input).toStrictEqual({error: 'error'})
@@ -32,9 +40,7 @@ test('Test if start value is greater than total number of messages', () => {
 
 // If the channelId is valid but authorised user is not a member
 test('Test authId that is not a member of the channel', () => {
+    const checkIfAuthMember = channelDetailsV1(3, 1);
     const input = channelMessagesV1(1, 1, 0);
-    const checkIfAuthMember = channelDetailsV1(2, 1);
-    
-    // Checking if authId is not in the allMembers array
-    expect(input).toEqual(expect.not.arrayContaining(checkIfAuthMember));
+    expect(input).toStrictEqual({error: 'error'})
 });
