2385a4f445efe58947da55cdccdbd2266099a595
Manav Pawar <z5413673@ad.unsw.edu.au>
2022-06-21 13:44:46 +1000

    finished details

--- src/channel.js
+++ src/channel.js
@@ -1,3 +1,7 @@
+import { getData } from './dataStore.js'
+
+const error = { error: 'error' };
+
 function channelJoinV1(authUserId, channelId) {
     return 'authUserId' + 'channelId';
 }
@@ -7,9 +11,33 @@ function channelInviteV1(authUserId, channelId, uId) {
 }
 
 function channelDetailsV1(authUserId, channelId) {
-    return 'authUserId' + 'channelId';
+
+    const data = getData();
+    
+    if ((data.channels).includes(channelId) === false) {
+        return error;
+    }
+      
+    if ((data.users[authUserId].channels).includes(channelId) === true) {
+        
+        let status = data.channels[channelId].isPublic;
+        let owner = data.channels[channelId].ownerMembers;
+        let all = data.channels[channelId].allMembers;
+           
+        return { 
+            name: name,
+            isPublic: isPublic,
+            ownerMembers: owner,
+            allMembers: all,
+        };
+    }
+  
+    return error;
 }
 
 function channelMessagesV1(authUserId, channelId, start) {
     return 'authUserId' + 'channelId' + 'start';
 }
+
+
+export { channelJoinV1, channelDetailsV1 };
