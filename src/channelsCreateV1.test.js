259ff8ef899731763e4f66faf70538f99d5815ff
Zhou Kevin Yu <Zhoukevinyu1@gmail.com>
2022-06-23 13:03:49 +1000

    implemented tests for channelsCreateV1 which involve channelDetails

--- src/channelsCreateV1.test.js
+++ src/channelsCreateV1.test.js
@@ -19,24 +19,24 @@ describe ('Testing return values', () => {
     test('valid channel name return value', () => {
         name = '1531';
         const noErrorOutput = { channelId: 0 }
-        expect(channelsCreateV1(authUserId, name, isPublic)).toEqual(noErrorOutput);
+        expect(channelsCreateV1(authUserId, name, isPublic)).toStrictEqual(noErrorOutput);
         name = '2'
         let output = { channelId: 1 }
-        expect(channelsCreateV1(authUserId, name, isPublic)).toEqual(output);
+        expect(channelsCreateV1(authUserId, name, isPublic)).toStrictEqual(output);
         name = '3'
         output = { channelId: 2 };
-        expect(channelsCreateV1(authUserId, name, isPublic)).toEqual(output);
+        expect(channelsCreateV1(authUserId, name, isPublic)).toStrictEqual(output);
     });
 
     //invalid name tests
     const errorOutput = { error: 'error' };
     test('invalid channel name with less than 1 character return value', () => {
         name = '';
-        expect(channelsCreateV1(authUserId, name, isPublic)).toEqual(errorOutput);
+        expect(channelsCreateV1(authUserId, name, isPublic)).toStrictEqual(errorOutput);
     });
     test('invalid channel name with more than 20 chars return value', () => {
         name = '1234567891011121314151617181920';
-        expect(channelsCreateV1(authUserId, name, isPublic)).toEqual(errorOutput);
+        expect(channelsCreateV1(authUserId, name, isPublic)).toStrictEqual(errorOutput);
     })
 });
 
@@ -52,34 +52,35 @@ describe ('Testing channel creation', () => {
         const {authUserId} = authRegisterV1('gary.sun@gmail.com', '1b2#XPS', 'Gary', 'Sun');
         name = '1531';
         channelsCreateV1(authUserId, name, isPublic);
-        expect(channelsListV1(authUserId)).toEqual(output);
+        expect(channelsListV1(authUserId)).toStrictEqual(output);
     });
 });
 
-/*describe ('Testing channel details', () => {
-    const user = userProfileV1(authUserId, authUserId);
-    let output = {
-        name:           '1531',
-        isPublic:       true,
-        ownerMembers:   [user],
-        allMembers:     [user]
-    };
+describe ('Testing channel details', () => {
     test('Public channel called "1531"', () => {
+        const {authUserId} = authRegisterV1('gary.sun@gmail.com', '1b2#XPS', 'Gary', 'Sun');
+        const user = userProfileV1(authUserId, authUserId);
+        name = '1531';
+        isPublic = true;
         channelsCreateV1(authUserId, name, isPublic);
-        expect(channelDetailsV1(authUserId, 0)).toEqual(output);
+        expect(channelDetailsV1(authUserId, 0)).toStrictEqual({
+            name:           '1531',
+            isPublic:       true,
+            ownerMembers:   [user],
+            allMembers:     [user]
+        });
     });
-    output = {
-        name:           'test channel',
-        isPublic:       false,
-        ownerMembers:   [user],
-        allMembers:     [user]
-    };
     test('Private channel called "test channel"', () => {
+        const {authUserId} = authRegisterV1('gary.sun@gmail.com', '1b2#XPS', 'Gary', 'Sun')
+        const user = userProfileV1(authUserId, authUserId);
+        name = 'test channel';
+        isPublic = false;
         channelsCreateV1(authUserId, name, isPublic);
-<<<<<<< HEAD
-        expect(channelDetailsV1(authUserId, 0)).toEqual(output);
-=======
-        expect(channelDetailsV1(authUserId, 1)).toEqual(output);
->>>>>>> 3542d290ffb7c3ab3d97fda40293d65d31c049ee
+        expect(channelDetailsV1(authUserId, 0)).toStrictEqual({
+            name:           'test channel',
+            isPublic:       false,
+            ownerMembers:   [user],
+            allMembers:     [user]
+        });
     })
-});*/
+});
