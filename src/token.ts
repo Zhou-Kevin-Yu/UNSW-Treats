eb312771f9fd7712339e252356bcc0908d165600
etkintetik <etkintetik101@gmail.com>
2022-07-13 06:31:01 +1000

    token converstion working

--- src/token.ts
+++ src/token.ts
@@ -1,6 +1,26 @@
-function tokenToAuthUserId(token: string, tokenValid: boolean): number{
+function tokenToAuthUserId(token: string, tokenValid: boolean): number {
+  if (!tokenValid) {
+    return null;
+  }
+  // Check if token is a float
+  // This could be improved later
+  if (token.includes('.')) {
+    const testToken = token.replace('.', '');
+    // Check if remaining is a number
+    if ((testToken.match(/^\d+$/)) === null) {
+      return null;
+    }
+  } else {
+    return null;
+  }
 
-    return null
+  const tokenSplit = token.split('.');
+  if (tokenSplit.length !== 2) {
+    return null;
+  }
+  const authUserId = parseInt(tokenSplit[0]);
+
+  return authUserId;
 }
 
 export { tokenToAuthUserId };
