d8f65fa4f5e11b198e1982ff90de56b3f547f64a
benkerno <benkerno1@gmail.com>
2022-07-13 17:02:10 +1000

    added creation of user through server

--- src/root.dm.test.ts
+++ src/root.dm.test.ts
@@ -2,6 +2,7 @@ import request from 'sync-request';
 import config from './config.json';
 
 import os from 'os';
+import { register } from 'ts-node';
 
 const OK = 200;
 const port = config.port;
@@ -13,9 +14,42 @@ if (os.platform() === 'darwin') {
   url = 'http://localhost';
 }
 
+beforeEach(() => {
+  request('DELETE',`${url}:${port}/clear/v1`);
+});
+
 console.log(url);
 describe('HTTP tests for dm/create', () => {
   describe('Testing Error Cases', () => {
+    test('Invalid uId', () => {
+      let res = request('POST',`${url}:${port}/auth/register/v2`, {
+        json: {
+          email: "benkerno@gmail.com",
+          password: "validPass23",
+          nameFirst: "ben",
+          nameLast: "kernohan"
+        }
+      });
+      let registerObj = JSON.parse(res.body as string);
+      let token = registerObj.token;
+      
+      res = request('POST',`${url}:${port}/dm/create/v1`, {
+        json: {
+          token: token,
+          uIds: [1], //this is an invalid uId
+        }
+      });
+    });
+    test('duplicate uIds invalid uId', () => {
+      request('POST',`${url}:${port}/auth/register/v2`, {
+        json: {
+          email: "benkerno@gmail.com",
+          password: "validPass23",
+          nameFirst: "ben",
+          nameLast: "kernohan"
+        }
+      });
+    });
     test('Test successful echo', () => {
       const res = request(
         'POST',
