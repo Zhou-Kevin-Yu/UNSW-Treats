7f3ae63791ffcd432d3dafd7edd4e8c1b5cef96c
Manav Pawar <z5413673@ad.unsw.edu.au>
2022-07-13 16:42:06 +1000

    added http testing

--- src/root.channels.test.ts
+++ src/root.channels.test.ts
@@ -1,87 +1,143 @@
-
+import { appendFile } from "fs";
 import { clearV1 } from "./other";
 import { authRegisterV2 } from './auth';
 import { channelsCreateV1, channelsListV1, channelsListallV1 } from "./channels";
+import { tokenToAuthUserId } from "./token";
+import { userProfileV2 }    from '../../user/profile/v2'
+import { channel } from "diagnostics_channel";
 
+const request = require('sync-request');
 const errorOutput = {error: "error"}
 
 beforeEach(() => {
     clearV1();
 });
 
-
-describe('Invalid inputs for channelsCreateV2', () => {
-    test('test invalid name (less than 1 chacaracter)', () => {
-
-      let token = authRegisterV2(gary.sun@gmail.com, password, gary, sun).token
-      let name = '';
-      let isPublic = true;
-      expect(channelCreateV2(token, name, isPublic)).toBe(errorOutput);
-    });
-
-    test('test invalid name (more than 20 chacaracters)', () => {
-
+describe('HTTP tests channelsCreateV2', () => {
+    test('Valid testing', () => {
         let token = authRegisterV2(gary.sun@gmail.com, password, gary, sun).token
-        let name = 'COMP1241241232141242142141233243121fesadad';
-        let isPublic = true;
-        expect(channelCreateV2(token, name, isPublic)).toBe(errorOutput);
-      });
-
-      test('test token (token doesnt exist)', () => {
-
-        let token = '-42354213basfhidwied';
         let name = 'COMP1531';
-        let isPublic = true;
-        expect(channelCreateV2(token, name, isPublic)).toBe(errorOutput);
-      });
+        const res = req (
+            'POST',
+                '${url}:${port}/channelsCreateV2',
+                {
+                    qs: {
+                        token: token,
+                        name: name,
+                        isPublic: true
+                    }
+                }
 
-  });
+        )
+    });
+    expect(res.statusCode).toBe(200);
+    expect(channels[0]).toBe(res.bodyObj);
+       
+});
+
+describe('HTTP tests channelsListV2', () => {
+    test('Valid testing', () => {
+        let token = authRegisterV2(gary.sun@gmail.com, password, gary, sun).token
+        let channel = channelCreateV2(token, COMP1531, isPublic)
+        const res = req (
+            'GET',
+                '${url}:${port}/channelsListV2',
+                {
+                    qs: {
+                        token: token
+                    }
+                }
+
+        )
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.bodyObj).toStrictEqual(
+        { channels: [
+            {
+                channelId: channel.channelId,
+                name: 'COMP1531'
+            }
+        ]
+        }
+
+    );
+       
+});
+
+describe('HTTP tests channelsListAllV2', () => {
+    test('Valid testing', () => {
+
+    let token = authRegisterV2('gary.sun@gmail.com', 'password', 'gary', 'sun').token
+    let channel = channelCreateV2(token, 'COMP1531', true)
+    let token1 = authRegisterV2('jeff.bezos@gmail.com', 'jesspassword','jeff' , 'bezos').token
+    let channel1 = channelCreateV2(token1, 'COMP1521', true)
+    const res = req (
+        'GET',
+            '${url}:${port}/channelsListAllV2',
+            {
+                qs: {
+                    token: token
+                }
+            }
+
+        )
+    });
+    expect(res.statusCode).toBe(200);
+    expect(res.bodyObj).toStrictEqual(
+        { channels: [
+            {
+                channelId: channel.channelId,
+                name: 'COMP1531'
+            },
+            {
+                channelId: channel1.channelId,
+                name: 'COMP1521'
+            }
+        ]
+        }
+
+    );
+      
+       
+});
+
+
+
   
 
-  describe('Valid inputs for channelsCreateV2', () => {
 
-    test('everything valid', () => {
+  
 
-        let token = authRegisterV2(gary.sun@gmail.com, password, gary, sun)
-        let name = 'COMP1531';
-        let isPublic = true;
-        let cID = channelCreateV2(token.token, name, isPublic).channelId;
-        let token1 = authRegisterV2(jeff.bezos@gmail.com, password54, jeff, bezos)
-        let name1 = 'COMP1521';
-        let isPublic1 = true;
-        let cID1 = channelCreateV2(token1.token, name1, isPublic).channelId1;
-        expect(channels[0].channelId).toBe(cID);
-        expect(channels[1].channelId).toBe(cID1);
-        expect(cID.channelId).toBe(0);
-        expect(cID1.channelId).toBe(1);
-        
-      });
+  ////////////////////////////////////////////////////////
 
-  });
 
-  describe('Invalid inputs for channelsListV2', () => {
-    test('test invalid name (less than 1 chacaracter)', () => {
+  //wrapper functions
 
-      let token = authRegisterV2(gary.sun@gmail.com, password, gary, sun).token
-      let name = '';
-      let isPublic = true;
-      expect(channelCreateV2(token, name, isPublic)).toBe(errorOutput);
-    });
+app.post('channels/create/V2', (req, res) = > {
 
-    test('test invalid name (more than 20 chacaracters)', () => {
+    const { token, name, isPublic } = req.body;
+    const authId = tokenToAuthUserId(token).authUserId;
+    const channelId = channelsCreateV1(authId, name, isPublic);
+    res.json(channelId);
 
-        let token = authRegisterV2(gary.sun@gmail.com, password, gary, sun).token
-        let name = 'COMP1241241232141242142141233243121fesadad';
-        let isPublic = true;
-        expect(channelCreateV2(token, name, isPublic)).toBe(errorOutput);
-      });
+});
 
-      test('test token (token doesnt exist)', () => {
+app.get('channels/List/V2', (req, res) = > {
 
-        let token = '-42354213basfhidwied';
-        let name = 'COMP1531';
-        let isPublic = true;
-        expect(channelCreateV2(token, name, isPublic)).toBe(errorOutput);
-      });
+    const token = req.query;
 
-  });
+    const channels = channelsListV1(token);
+
+    return res.json(channels);
+});
+
+app.get('channels/ListAll/V2', (req, res) = > {
+
+    const token = req.query;
+
+    const channels = channelsListV1(token);
+
+    return res.json(channels);
+});
+
+
