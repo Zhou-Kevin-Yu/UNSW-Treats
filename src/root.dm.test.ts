d5e0822827feed9f698ee2cdd062f6a031fe5280
benkerno <benkerno1@gmail.com>
2022-07-13 17:30:12 +1000

    added an error case for when creator is in the uId list & a basic success case for one created dm

--- src/root.dm.test.ts
+++ src/root.dm.test.ts
@@ -36,7 +36,7 @@ describe('HTTP tests for dm/create', () => {
       res = request('POST',`${url}:${port}/dm/create/v1`, {
         json: {
           token: token,
-          uIds: [1], //invalid uId
+          uIds: [23], //invalid uId
         }
       });
       const returnObj = JSON.parse(res.body as string);
@@ -81,7 +81,86 @@ describe('HTTP tests for dm/create', () => {
     expect(returnObj).toBe({error: 'error'})
     });
 
-    
+
+    test('Creator is also in the uId Array', () => {
+      //create a valid register request
+      let res1 = request('POST',`${url}:${port}/auth/register/v2`, {
+        json: {
+          email: "benkerno@gmail.com",
+          password: "validPass23",
+          nameFirst: "ben",
+          nameLast: "kernohan"
+        }
+      });
+      let res1Obj = JSON.parse(res1.body as string);
+      let token1 = res1Obj.token;
+      let uId1 = res1Obj.authUserId;
+
+      //create a second valid register request
+      let res2 = request('POST',`${url}:${port}/auth/register/v2`, {
+        json: {
+          email: "benkerno1@gmail.com",
+          password: "validPass23",
+          nameFirst: "ben",
+          nameLast: "kernohan"
+        }
+      });
+      let res2Obj = JSON.parse(res2.body as string);
+      let uId2 = res1Obj.authUserId;
+
+      //create request that should return an error
+      let res = request('POST',`${url}:${port}/dm/create/v1`, {
+        json: {
+          token: token1,
+          uIds: [uId1, uId2], //uId1 is the creator
+        }
+      });
+    const returnObj = JSON.parse(res.body as string);
+    expect(returnObj).toBe({error: 'error'})
+    });
+  });
+
+  describe('Testing Success Cases', () => {
+
+    test('creation of one dm', () => {
+      //create a valid register request
+      let res1 = request('POST',`${url}:${port}/auth/register/v2`, {
+        json: {
+          email: "benkerno@gmail.com",
+          password: "validPass23",
+          nameFirst: "ben",
+          nameLast: "kernohan"
+        }
+      });
+      let res1Obj = JSON.parse(res1.body as string);
+      let token1 = res1Obj.token;
+      let uId1 = res1Obj.authUserId;
+
+      //create a second valid register request
+      let res2 = request('POST',`${url}:${port}/auth/register/v2`, {
+        json: {
+          email: "benkerno1@gmail.com",
+          password: "validPass23",
+          nameFirst: "ben",
+          nameLast: "kernohan"
+        }
+      });
+      let res2Obj = JSON.parse(res2.body as string);
+      let uId2 = res1Obj.authUserId;
+
+      //create request that should be successful
+      let res = request('POST',`${url}:${port}/dm/create/v1`, {
+        json: {
+          token: token1,
+          uIds: [uId2], //uId2 is duplicated twice
+        }
+      });
+    const returnObj = JSON.parse(res.body as string);
+    expect(returnObj.dmId).toBe(0); //think this has to stay at 0?;
+    });
+  });
+
+    /*
     test('Test successful echo', () => {
       const res = request(
         'POST',
@@ -97,6 +176,6 @@ describe('HTTP tests for dm/create', () => {
       expect(res.statusCode).toBe(OK);
       expect(bodyObj).toEqual('Hello');
     });
-      
-  });
+    */
+ 
 });
