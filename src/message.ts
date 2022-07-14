40e01833edbe7d6ef68ffa6b84ea8cf853cb6275
Calvin Xu <z5359028@ad.unsw.edu.au>
2022-07-14 16:55:05 +1000

    Fixed a logic error in message/senddm/v1

--- src/message.ts
+++ src/message.ts
@@ -16,6 +16,7 @@ function messageSendV1 (token: string, channelId: number, message: string) { // 
   const data = getData();
   let existChannel = 0;
   let existAuth = 0;
+  let messageIdCopy = 0;
   // If token is invalid
   if (!isTokenValid(token)) {
     return error;
@@ -39,7 +40,7 @@ function messageSendV1 (token: string, channelId: number, message: string) { // 
         if (authUserId === member.uId) {
           existAuth = 1;
 
-          let messageIdCopy = data.systemInfo.messageTotal;
+          messageIdCopy = data.systemInfo.messageTotal;
           data.systemInfo.messageTotal++;
           const newChannelMessage: MessagesObj = {
             messageId: messageIdCopy,
@@ -76,104 +77,104 @@ function messageSendV1 (token: string, channelId: number, message: string) { // 
  */
 function messageEditV1 (token: string, messageId: number, message: string) {
   
-  const data = getData();
-  let existMessage = 0;
-  let existAuth = 0;
-  let existOwner = 0;
-  // If token is invalid
-  if (!isTokenValid(token)) {
-    return error;
-  }
+  // const data = getData();
+  // let existMessage = 0;
+  // let existAuth = 0;
+  // let existOwner = 0;
+  // // If token is invalid
+  // if (!isTokenValid(token)) {
+  //   return error;
+  // }
 
-  // If length of message is over 1000 characters
-  if (message.length > 1000) {
-    return error;
-  }
+  // // If length of message is over 1000 characters
+  // if (message.length > 1000) {
+  //   return error;
+  // }
 
-  // i need to loop through channel.messages and dm.mesages
+  // // i need to loop through channel.messages and dm.mesages
 
-  const authUserId = tokenToAuthUserId(token, isTokenValid(token));
+  // const authUserId = tokenToAuthUserId(token, isTokenValid(token));
 
-  // For editing messages in channel, loop through all the existing channels
-  for (const channel of data.channels) {
-    // Loop through all messages 
-    for (const msg of channel.messages) {
-      // If messageId exists
-      if (msg.messageId === messageId) {
-        existMessage = 1;
-        // If message was sent by the auth user making this edit request
-        if (msg.uId === authUserId) {
-          existAuth = 1;
-          // Loop through users
-          for (user of data.users) {
-            // Check if they have global owner permissions 
-            if (user.uId === authuserId && user.permission === 1) {
-              existOwner = 1;
-            }
-          }
+  // // For editing messages in channel, loop through all the existing channels
+  // for (const channel of data.channels) {
+  //   // Loop through all messages 
+  //   for (const msg of channel.messages) {
+  //     // If messageId exists
+  //     if (msg.messageId === messageId) {
+  //       existMessage = 1;
+  //       // If message was sent by the auth user making this edit request
+  //       if (msg.uId === authUserId) {
+  //         existAuth = 1;
+  //         // Loop through users
+  //         for (user of data.users) {
+  //           // Check if they have global owner permissions 
+  //           if (user.uId === authuserId && user.permission === 1) {
+  //             existOwner = 1;
+  //           }
+  //         }
 
-          // Check if they are an owner of the channel
-          if () {
+  //         // Check if they are an owner of the channel
+  //         if () {
 
-          }
+  //         }
 
-          // const editChannelMessage: MessagesObj = {
-          //   messageId: messageId,
-          //   uId: authUserId,
-          //   message: message,
-          //   timeSent: 0,
-          // };
+  //         // const editChannelMessage: MessagesObj = {
+  //         //   messageId: messageId,
+  //         //   uId: authUserId,
+  //         //   message: message,
+  //         //   timeSent: 0,
+  //         // };
           
-          // wait it is not push just replacing the message
-          // ask about this 
+  //         // wait it is not push just replacing the message
+  //         // ask about this 
 
-          // I am confused about permissions so users can edit their own messages and channel owners can?
-          // doesnt that contract this statement 
-          // "the message was not sent by the authorised user making this request"??
-          // "You should return an error if an authorised user tries to edit someone else's message, and they don't have owner permissions.
+  //         // I am confused about permissions so users can edit their own messages and channel owners can?
+  //         // doesnt that contract this statement 
+  //         // "the message was not sent by the authorised user making this request"??
+  //         // "You should return an error if an authorised user tries to edit someone else's message, and they don't have owner permissions.
 
-          // If the new message is an empty string, the message is deleted
-          // how do i do this
-          if (message.length === 0) {
-            delete data.channels.messages[messageId];
-          }
+  //         // If the new message is an empty string, the message is deleted
+  //         // how do i do this
+  //         if (message.length === 0) {
+  //           delete data.channels.messages[messageId];
+  //         }
 
-          setData(data);
-          // return { };
+  //         setData(data);
+  //         // return { };
 
-        }
-      }
-    }
-  }
+  //       }
+  //     }
+  //   }
+  // }
 
-  // If messageId does not refer to a valid message within a channel/DM that the authorised user has joined
-  if (existMessage === 0) {
-    return error;
-  }
+  // // If messageId does not refer to a valid message within a channel/DM that the authorised user has joined
+  // if (existMessage === 0) {
+  //   return error;
+  // }
 
-  // If message was not sent by the authorised user making this edit request
-  if (existAuth === 0) {
-    return error;
-  }
+  // // If message was not sent by the authorised user making this edit request
+  // if (existAuth === 0) {
+  //   return error;
+  // }
 
-  // If the authorised user does not have owner permissions in the channel/DM
-  // The only users with owner permissions in DMs are the original creators of each DM.
-  if (existOwner === 0) {
-    return error;
-  }
+  // // If the authorised user does not have owner permissions in the channel/DM
+  // // The only users with owner permissions in DMs are the original creators of each DM.
+  // if (existOwner === 0) {
+  //   return error;
+  // }
 
-  // implementation down below
-  // the logic is to loop through the channels
-  // loop through all members in selected channel
-  // if member token not the same return error
-  // if message ID never recgosnied return error
-  // if permission Id not the same returne error
-  // then can now implement
-  // have new text replace old text using some sort of function
-  // maybe spacer function [...array] spread opeartor
-  // say if new message in parameter is an empty string, then use some form of message = null or message id = null
+  // // implementation down below
+  // // the logic is to loop through the channels
+  // // loop through all members in selected channel
+  // // if member token not the same return error
+  // // if message ID never recgosnied return error
+  // // if permission Id not the same returne error
+  // // then can now implement
+  // // have new text replace old text using some sort of function
+  // // maybe spacer function [...array] spread opeartor
+  // // say if new message in parameter is an empty string, then use some form of message = null or message id = null
 
-  return { };
+  // return { };
 }
 
 /**
@@ -224,6 +225,7 @@ function messageSendDmV1 (token: string, dmId: number, message: string) {
   const data = getData();
   let existDm = 0;
   let existAuth = 0;
+  let messageIdCopy = 0;
   // If token is invalid
   if (!isTokenValid(token)) {
     return error;
@@ -242,12 +244,13 @@ function messageSendDmV1 (token: string, dmId: number, message: string) {
     if (dmId === dm.dmId) {
       existDm = 1;
       // To loop through all the members in selected DM
-      for (const member of dm.members) {
+      // for (const member of dm.members) {
+      for (let i = 0; i < dm.members.length; i++) {
         // If the auth user is a member of DM
-        if (authUserId === member.uId) {
+        if (dm.members[i] === authUserId) {
           existAuth = 1;
 
-          let messageIdCopy = data.systemInfo.messageTotal;
+          messageIdCopy = data.systemInfo.messageTotal;
           data.systemInfo.messageTotal++;
 
           const newDmMessage: MessagesObj = {
