a58b602dab830aac4df1b4d4da220c5ed354b1a6
cs1531 <cs1531@cse.unsw.edu.au>
2022-07-17 15:12:11 +1000

    Results push

--- src/other.ts
+++
@@ -1,9 +0,0 @@
-import { getData, setData } from './dataStore';
-
-function clearV1() {
-  let data = getData();
-  data = { users: [], channels: [], dms: [], systemInfo: { messageTotal: 0 } };
-  setData(data);
-}
-
-export { clearV1 };
