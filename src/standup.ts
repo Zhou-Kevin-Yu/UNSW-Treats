cd14ffc7b3bf62513f9b3fca3e828811bb4a42f1
etkintetik <etkintetik101@gmail.com>
2022-08-04 16:34:07 +1000

    added stubs for standup

---
+++ src/standup.ts
@@ -0,0 +1,18 @@
+
+function standupStartV1 (channelId: number, length: number): number{
+
+    return 0;
+}
+
+function standupActiveV1 (channelId: number){
+
+    return {
+        isActive: false,
+        timeFinish: 0,
+    };
+};
+
+function standupSendV1 (channelId: number, message: string) {
+
+    return {};
+}
