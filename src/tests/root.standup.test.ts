101e9a9b40135e2a73d49a18e1976f2496334c3c
etkintetik <etkintetik101@gmail.com>
2022-08-04 22:56:35 +1000

    made a file for standup http tests

---
+++ src/tests/root.standup.test.ts
@@ -0,0 +1,20 @@
+// import request from 'sync-request';
+// import config from '../config.json';
+// import os from 'os';
+
+// import { wrappedStandupStartServerSide, wrappedStandupActiveServerSide, wrappedStandupSendServerSide } from '../wrapped.standup';
+
+// import { authLoginV2ServerSide, authRegisterV2ServerSide } from '../wrapped.auth';
+
+
+// const OK = 200;
+// const port = config.port;
+// let url = config.url;
+
+// if (os.platform() === 'darwin') {
+//   url = 'http://localhost';
+// }
+
+// describe('/standup/start/v1', () => {
+    
+// });
