63fd244b56d5ca95737c0c97ad4a41b42c91715a
Calvin Xu <z5359028@ad.unsw.edu.au>
2022-08-05 21:15:13 +1000

    Deleted comments

--- src/notifications.ts
+++ src/notifications.ts
@@ -21,107 +21,24 @@ import HTTPError from 'http-errors';
 export function notificationsGetV1 (token: string)/*: NotificationsGetV1*/ {
 
   const data = getData();
-  // let channelIdCopy = -1;
-  // let dmIdCopy = -1;
-  // let notificationMsgCopy = '';
   let notificationArray: NotificationsObj[] = [];
-  // // let index = 0; 
 
   // If token is invalid
-  if (!isTokenValid(token)) {
-    throw HTTPError(403, "token passed in is invalid");
-  }
+  // if (!isTokenValid(token)) {
+  //   throw HTTPError(403, "token passed in is invalid");
+  // }
 
   const authUserId = tokenToAuthUserId(token, isTokenValid(token));
-  
-  // const newNotification: NotificationsObj = {
-  //   channelId: channelIdCopy,
-  //   dmId: dmIdCopy,
-  //   notificationMessage: notificationMsgCopy,
-  // };
 
-  // // Loop through reacted message in channels
-  // for (const channel of data.channels) {
-  //   for (const message of channel.messages) {
-      
-  //     // Fix this below
-  //     const userHandle = userProfileV2(token, message.uId).user.handleStr;
-
-  //     // do i need to do message.reacts[].reactId === 1
-  //     if (message.reacts != null) {
-  //       channelIdCopy = channel.channelId;
-  //       notificationMsgCopy = `${userHandle} reacted to your message in ${channel.name}`;
-        
-  //       // data.notifications[index].push(newNotification);
-  //       data.notifications.push(newNotification);
-  //       setData(data);
-  //       // index++;
-  //     }
-  //   }
-  // }
-
-  // // Loop through reacted message in DMs
-  // for (const dm of data.dms) {
-  //   for (const message of dm.messages) {
-      
-  //     // Fix this below
-  //     const userHandle = userProfileV2(token, message.uId).user.handleStr;
-
-  //     // do i need to do message.reacts[].reactId === 1
-  //     if (message.reacts != null) {
-  //       dmIdCopy = dm.dmId;
-  //       notificationMsgCopy = `${userHandle} reacted to your message in ${dm.name}`;
-
-  //       // data.notifications[index].push(newNotification);
-  //       data.notifications.push(newNotification);
-  //       setData(data);
-  //       // index++;
-  //     }
-  //   }
-  // }
-
-  // // Loop through tagged message in channels
-  
-
-  // // Loop through tagged message in DMs
-
-  // // Need to implement added to channel and added to DM
-
-  // // const newNotification: NotificationsObj = {
-  // //   channelId: channelIdCopy,
-  // //   dmId: dmIdCopy,
-  // //   notificationMessage: notificationMsgCopy,
-  // // };
-
-  // // data.notifications[0].push(newNotification);
-  // // notificationArray = data.notifications[].push(newNotification);
   for (const user of data.users) {
+    //console.log('hi');
     if (user.uId === authUserId) {  
-      // notificationArray = user.notifications.reverse();
-      // notificationArray.reverse(user.notifications);
+      // console.log('hi');
       notificationArray = notificationArray.concat(user.notifications);
       notificationArray.reverse();
       notificationArray = notificationArray.slice(0, 20);
     }
   }
-  // // setData(data);
-  // // notificationArray.reverse();
-  
-  // // Loop to push messages into msgArray
-  // // for (let i = startCopy; i < endCopy; i++) {
-  // //   msgArray.push(channel.messages[i]);
-  // // }
 
-
-
-  // // use for loops and if statements
-  // // to locate if occured in channel id yes or not
-  // // to locate if occured in dm yes or not
-  // // tagged is the actual string
-  // // reacted message is actual string
-  // // added to a channel/dm is actual string
-  // // need to find channel/dm name, message string and user's handle
-
-  // return {};
   return notificationArray;
 }
