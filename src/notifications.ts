42b7eaa5df90be443a10d90e36fe6f579b0d5c3d
Calvin Xu <z5359028@ad.unsw.edu.au>
2022-08-05 16:27:32 +1000

    Partially implemented some of notifications/get/v1 but still facing some errors

--- src/notifications.ts
+++ src/notifications.ts
@@ -1,6 +1,7 @@
-import { getData } from './dataStore';
+import { getData, setData } from './dataStore';
 import { tokenToAuthUserId, isTokenValid } from './token';
-import { NotificationsObj, NotificationsGetV1 } from './dataStore';
+import { userProfileV2 } from './user';
+import { NotificationsObj, /*NotificationsGetV1*/ } from './dataStore';
 import HTTPError from 'http-errors';
 
 // Array of objects, where each object contains types { channelId, dmId, notificationMessage } where
@@ -17,26 +18,85 @@ import HTTPError from 'http-errors';
  * Return the user's most recent 20 notifications, ordered from most recent to least recent.
  * @returns 
  */
-export function notificationsGetV1 (token: string): NotificationsGetV1 {
+export function notificationsGetV1 (token: string)/*: NotificationsGetV1*/ {
 
   const data = getData();
+  let channelIdCopy = -1;
+  let dmIdCopy = -1;
+  let notificationMsgCopy = '';
+  let notificationArray: NotificationsObj[] = [];
+  // let index = 0; 
+
   // If token is invalid
   if (!isTokenValid(token)) {
     throw HTTPError(403, "token passed in is invalid");
   }
 
   const authUserId = tokenToAuthUserId(token, isTokenValid(token));
-
   
+  const newNotification: NotificationsObj = {
+    channelId: channelIdCopy,
+    dmId: dmIdCopy,
+    notificationMessage: notificationMsgCopy,
+  };
 
-  // let notificationArray: NotificationsObj[] = [];
+  // Loop through reacted message in channels
+  for (const channel of data.channels) {
+    for (const message of channel.messages) {
+      
+      // Fix this below
+      const userHandle = userProfileV2(token, message.uId).user.handleStr;
+
+      // do i need to do message.reacts[].reactId === 1
+      if (message.reacts != null) {
+        channelIdCopy = channel.channelId;
+        notificationMsgCopy = `${userHandle} reacted to your message in ${channel.name}`;
+        
+        // data.notifications[index].push(newNotification);
+        data.notifications.push(newNotification);
+        setData(data);
+        // index++;
+      }
+    }
+  }
+
+  // Loop through reacted message in DMs
+  for (const dm of data.dms) {
+    for (const message of dm.messages) {
+      
+      // Fix this below
+      const userHandle = userProfileV2(token, message.uId).user.handleStr;
+
+      // do i need to do message.reacts[].reactId === 1
+      if (message.reacts != null) {
+        dmIdCopy = dm.dmId;
+        notificationMsgCopy = `${userHandle} reacted to your message in ${dm.name}`;
+
+        // data.notifications[index].push(newNotification);
+        data.notifications.push(newNotification);
+        setData(data);
+        // index++;
+      }
+    }
+  }
+
+  // Loop through tagged message in channels
+
+  // Loop through tagged message in DMs
+
+  // Need to implement added to channel and added to DM
+
   // const newNotification: NotificationsObj = {
-  //   channelId: ,
-  //   dmId: ,
-  //   notificationMessage: ,
+  //   channelId: channelIdCopy,
+  //   dmId: dmIdCopy,
+  //   notificationMessage: notificationMsgCopy,
   // };
-  // 
-  // notificationArray = data.notifications.push(newNotification);
+
+  // data.notifications[0].push(newNotification);
+  // notificationArray = data.notifications[].push(newNotification);
+  notificationArray.reverse(data.notifications);
+  notificationArray = data.notifications.slice(0, 20);
+  // setData(data);
   // notificationArray.reverse();
   
   // Loop to push messages into msgArray
@@ -54,6 +114,6 @@ export function notificationsGetV1 (token: string): NotificationsGetV1 {
   // added to a channel/dm is actual string
   // need to find channel/dm name, message string and user's handle
 
-  return {};
-  // return notificationArray;
+  // return {};
+  return notificationArray;
 }
