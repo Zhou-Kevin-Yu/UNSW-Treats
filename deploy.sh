9eccc87ca30d1d06e78d58c72b55439f1eccab02
cs1531 <cs1531@cse.unsw.edu.au>
2022-08-11 21:19:31 +1000

    Results push

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-WORKING_DIRECTORY="~/www/cs1531deployass"
-
-USERNAME="z5359859"
-SSH_HOST="ssh-z5359859.alwaysdata.net"
-
-scp -r ./package.json ./package-lock.json ./tsconfig.json ./src "$USERNAME@$SSH_HOST:$WORKING_DIRECTORY"
-ssh "$USERNAME@$SSH_HOST" "cd $WORKING_DIRECTORY && npm install --only=production"
